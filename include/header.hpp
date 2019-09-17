d1fcb1c0040fa9cd898912948472babe8b6d3c95
IliaNekleenov <neiluha@mail.ru>
2019-09-17 13:08:18 +0300

    first try

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,111 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2019 IliaNekleenov <neiluha@mail.ru>
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
-#endif // INCLUDE_HEADER_HPP_
+#include <atomic>
+#include <stdexcept>
+
+class counter {
+  std::atomic<int> count;
+
+ public:
+  counter();
+  counter(int c);
+  void increase();
+  void decrease();
+  bool operator==(const int& a) const;
+  int get() const;
+};
+
+template <typename T>
+class SharedPtr {
+  counter* c_ptr;
+  T* data;
+
+ public:
+  SharedPtr() {
+    c_ptr = nullptr;
+    data = nullptr;
+  }
+  SharedPtr(T* p) {
+    c_ptr = new counter(1);
+    data = p;
+  }
+  SharedPtr(const SharedPtr& r) {
+    c_ptr = r.c_ptr;
+    if (r) c_ptr->increase();
+    data = r.data;
+  }
+  SharedPtr(const SharedPtr&& r) {
+    c_ptr = r.c_ptr;
+    if (r) c_ptr->increase();
+    data = r.data;
+  }
+  auto operator=(const SharedPtr& r) -> SharedPtr<T>& {
+    if (*this) {
+      c_ptr->decrease();
+      if (*c_ptr == 0) {
+        delete c_ptr;
+        delete data;
+      }
+    }
+    c_ptr = r.c_ptr;
+    data = r.data;
+  }
+  auto operator=(SharedPtr&& r) -> SharedPtr& {
+    if (*this) {
+      c_ptr->decrease();
+      if (*c_ptr == 0) {
+        delete c_ptr;
+        delete data;
+      }
+    }
+    c_ptr = r.c_ptr;
+    data = r.data;
+  }
+
+  operator bool() const { return !(data == nullptr); }
+  auto operator*() const -> T& {
+    if (!this) {
+      throw std::logic_error("attempt to get value of nullptr")
+    }
+    return *data;
+  }
+  auto operator-> () const -> T* { return data; }
+
+  auto get() -> T* { return data; }
+  void reset() {
+    if (!*this) return;
+    c_ptr->decrease();
+    if (*c_ptr == 0) {
+      delete c_ptr;
+      delete data;
+    }
+    c_ptr = nullptr;
+    data = nullptr;
+  }
+  void reset(T* ptr) {
+    if (*this) {
+      c_ptr->decrease();
+      if (*c_ptr == 0) {
+        delete c_ptr;
+        delete data;
+      }
+    }
+    c_ptr = new counter(1);
+    data = ptr;
+  }
+  void swap(SharedPtr& r) {
+    if (data == r.data) return;
+    auto buf = r;
+    r = *this;
+    *this = buf;
+  }
+  auto use_count() const -> size_t {
+    if (!this) return 0;
+    return static_cast<size_t>(c_ptr->get());
+  }
+};
+
+#endif  // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,61 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2019 IliaNekleenov <neiluha@mail.ru>
 
 #include <gtest/gtest.h>
 
 #include <header.hpp>
+
+TEST(SHARED_PTR, init) {
+  SharedPtr<int> ptr;
+  EXPECT_FALSE(ptr);
+  EXPECT_EQ(p.use_count(), 0);
+  EXPECT_THROW(*ptr, std::logic_error("attempt to get value of nullptr"));
+  int value = 5;
+  SharedPtr<int> p(&value);
+  EXPECT_EQ(p.use_count(), 1);
+  EXPECT_EQ(*p, 5);
+  EXPECT_TRUE(p);
+  SharedPtr<int> p1(p);
+  EXPECT_EQ(p.use_count(), p1.use_count());
+  EXPECT_EQ(p.use_count(), 2);
+  EXPECT_EQ(*p1, 5);
+  EXPECT_TRUE(p1);
+}
+
+TEST(SHARED_PTR, assign) {
+  int value = 8;
+  SharedPtr<int> p(&value);
+  int anoth_val = 2;
+  SharedPtr<int> p1(&anoth_val);
+  p = p1;
+  EXPECT_EQ(p.use_count(), p1.use_count());
+  EXPECT_EQ(p.use_count(), 2);
+  EXPECT_EQ(*p1, *p);
+  EXPECT_TRUE(p1);
+}
+
+TEST(SHARED_PTR, test_of_reset) {
+  int value = 3;
+  SharedPtr<int> p(&value);
+  SharedPtr<int> p1(p);
+  EXPECT_EQ(p.use_count(), 2);
+  p.reset();
+  EXPECT_FALSE(p);
+  EXPECT_EQ(p.use_count(), 0);
+  EXPECT_EQ(p1.use_count(), 1);
+  int val = 10;
+  p1.reset(&val);
+  EXPECT_EQ(p1.use_count(), 1);
+  EXPECT_EQ(*p1, 10);
+}
+
+TEST(SHARED_PTR, test_of_swap) {
+  int value = 3;
+  SharedPtr<int> p(&value);
+  int val = 10;
+  SharedPtr<int> p1(&val);
+  p1.swap(p);
+  EXPECT_EQ(p1.use_count(), 1);
+  EXPECT_EQ(p.use_count(), 1);
+  EXPECT_EQ(*p1, 3);
+  EXPECT_EQ(*p, 10);
+}
